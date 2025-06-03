851d13cdf47817008f1a3d8899e54e7ab592553d
Muatassim Aziz <muatassim@hotmail.com>
2025-06-02 20:03:39 -0400

    Added Templates with different Layers

--- Files/CoreTest/Data/DateTimeProvider.cs
+++
@@ -1,9 +0,0 @@
-using CleanArchitecture.Core.Interfaces;
-
-namespace CleanArchitecture.CoreTest.Data
-{
-    public sealed class DateTimeProvider : IDateTimeProvider
-    {
-        public DateTime UtcNow => DateTime.UtcNow;
-    }
-}
