599856666b637673618194419195c4a628380bd0
Muatassim Aziz <muatassim@hotmail.com>
2025-06-04 16:59:59 -0400

    remove not needed files

--- Template/Initializer/CleanArchitecture/Database/CleanArchitectureDb/bin/Debug/Scripts/Post-Deployment/users/LoginScript.sql
+++
@@ -1,7 +0,0 @@
-﻿Print 'Executing Login Scripts'
-IF EXISTS(SELECT * FROM sys.sql_logins WHERE name = 'CleanArchitectureDbUser')
-DROP LOGIN [CleanArchitectureDbUser]
-
-IF NOT EXISTS (SELECT * FROM sys.sql_logins WHERE name = N'CleanArchitectureDbUser')
-CREATE LOGIN [CleanArchitectureDbUser] WITH PASSWORD='$(UserPassword)'
-	Print 'End Executing Login Scripts'
--- Template/Initializer/CleanArchitecture/Database/CleanArchitectureDb/bin/Debug/Scripts/Post-Deployment/users/UserScript.sql
+++
@@ -1,8 +0,0 @@
-﻿-- this needs to run on the database not on master  database 
-
-IF EXISTS(SELECT * FROM sys.sysusers WHERE name = 'CleanArchitectureDbUser')
-  DROP USER [CleanArchitectureDbUser] 
-GO
-IF NOT EXISTS (SELECT * FROM sys.sysusers WHERE name = N'CleanArchitectureDbUser')
-  CREATE USER [CleanArchitectureDbUser] FROM LOGIN [CleanArchitectureDbUser] WITH DEFAULT_SCHEMA=dbo; 
-GO
